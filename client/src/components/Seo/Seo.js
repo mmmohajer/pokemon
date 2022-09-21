855e7b37587e9168bd3d14bbd2573f71f771f618
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-09-21 11:01:08 -0400

    The finalized release is ready

--- client/src/components/Seo/Seo.js
+++ client/src/components/Seo/Seo.js
@@ -12,7 +12,6 @@ const Seo = ({ title, keywords, description, children }) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       {children}
     </>
